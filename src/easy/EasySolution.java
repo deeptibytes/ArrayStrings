377486d16695254c9a924dc4f2ee1943c528ee06
dshukla <dshukla@marqeta.com>
2022-03-24 21:29:42 -0700

    3/24

--- src/easy/EasySolution.java
+++ src/easy/EasySolution.java
@@ -1,34 +1,36 @@
 package easy;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EasySolution {
 
-    /*
+    /**
       Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
      */
 
-    //Using set - Time = O(N)  Space = O(N)
-    public static boolean isUniqueStringSet(String input){
-         Set<Character> set = new HashSet<Character>();
+    //Using set
+    // Time = O(N)
+    // Space = O(N)
+    public static boolean isUniqueStringSet(String input) {
+        Set<Character> set = new HashSet();
 
-         for(int i = 0; i <input.length(); i++ ){
-             if(set.contains(input.charAt(i))){
-                 return false;
-             }
-             set.add(input.charAt(i));
+        for (int i = 0; i < input.length(); i++) {
+            if (set.contains(input.charAt(i))) {
+                return false;
+            }
+            set.add(input.charAt(i));
 
-         }
+        }
         return true;
     }
 
     //Using boolean array - Time = O(N)  Space = O(1)
-    public static boolean isUniqueStringArray(String input){
-        boolean[] arr = new boolean[127];
+    public static boolean isUniqueStringArray(String input) {
 
-        for(int i = 0; i <input.length(); i++ ){
-            if(  arr[input.charAt(i)]){
+        boolean[] arr = new boolean[128];
+
+        for (int i = 0; i < input.length(); i++) {
+            if (arr[input.charAt(i)]) {
                 return false;
             }
             arr[input.charAt(i)] = true;
@@ -39,27 +41,75 @@ public class EasySolution {
 
     //without using any DS - Time = O(N square)  Space = O(1)
     //abcaa    abcbd
-    public static boolean isUniqueStringNoDS(String input){
+    public static boolean isUniqueStringNoDS(String input) { //Brute force way
+        for (int i = 0; i < input.length() - 1; i++) {
+            for (int j = i + 1; j < input.length(); j++) {
 
-        int i;
-
-        for(i = 0; i < input.length() - 1; i++){
-            for(int j = i+1; j < input.length(); j++){
-
-                if(input.charAt(i) == input.charAt(j) ){
+                if (input.charAt(i) == input.charAt(j)) {
                     return false;
                 }
             }
 
         }
         return true;
-
     }
-  /*
+  /**
      Check Permutation: Given two strings,write a method to decide if one is a permutation of the other string.
   */
+    //Time = O(N)
+    //Space =O(N)
+    public static boolean isPermutation(String one, String two) {
+
+        if(one.length() != two.length()) return false;
+
+        Map<Character, Integer> map1 = new HashMap();
+        for(int i = 0 ; i < one.length(); i++){
+            Character charAti = one.charAt(i);
+            map1.put(charAti, map1.getOrDefault(charAti, 0) + 1);
+        }
+
+        for(int i = 0 ; i < two.length(); i++){
+            Character charAti = two.charAt(i);
+            if(! map1.containsKey(charAti))return false;
+            map1.put(charAti, map1.getOrDefault(charAti, 0) - 1);
+            if(map1.get(charAti) < 0) return false;
+        }
+        return true;
+    }
+    //Time = O(N)
+    //Space =O(1)
+    public static boolean isPermutationSeconddWay(String one, String two) {
+        if(one.length() != two.length()) return false;
+
+        int[] arr = new int[52];//since string contains only chars (english letters). If this is not given, then use 128
+        for(int i = 0 ; i < one.length(); i++){
+            Character charAti = one.charAt(i);
+            arr[charAti] = arr[charAti]++;
+        }
+
+        for(int i = 0 ; i < two.length(); i++){
+            Character charAti = one.charAt(i);
+            arr[charAti] = arr[charAti]--; //if count of char is same, it will be zero. If less than zero, means counts if char is more in two
+            if(arr[charAti] < 0) return false;
+        }
+
+        return true;
+    }
 
 
+    //Time = O(nLogn + mlogm)
+    //Space = O(1) ??
+    public static boolean isPermutationThirddWay(String one, String two) {
+        if(one.length() != two.length()) return false;
+        //sort both of them and then compare Sorting takes O(nLogn + mlogm) where n and m are the length of strings
+        return sort(one).equals(sort(two));
+    }
+
+    private static String sort(String s){
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        return arr.toString();
+    }
 
     /*
      URLify: Write a method to replace all spaces in a string with '%20'.
@@ -105,13 +155,11 @@ Hints:#92, #110
 
      */
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String str = "abcd";
-       //System.out.println(isUniqueStringNoDS(str));
+        //System.out.println(isUniqueStringNoDS(str));
         System.out.println(isUniqueStringArray(str));
-
-
 
 
     }
